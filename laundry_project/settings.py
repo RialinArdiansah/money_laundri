c54e997ed97fcd9a58cc4411738adb944576cd4f
RialinArdiansah <12350113034@students.uin-suska.ac.id>
2025-11-30 02:28:01 +0700

    Fix whitenoise storage configuration

--- laundry_project/settings.py
+++ laundry_project/settings.py
@@ -93,7 +93,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Whitenoise configuration for production
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
